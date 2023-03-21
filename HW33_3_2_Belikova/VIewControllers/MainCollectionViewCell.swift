79cfa9f860feda45d7e4cf8868f7942ac4553812
@Annabel_ <hydrant94@gmail.com>
2023-03-21 21:19:43 +0400

    basic elements

---
+++ HW33_3_2_Belikova/VIewControllers/MainCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MainCollectionViewCell.swift
+//  HW33_3_2_Belikova
+//
+//  Created by Аня Беликова on 21.03.2023.
+//
+
+import UIKit
+
+class MainCollectionViewCell: UICollectionViewCell {
+    
+}
